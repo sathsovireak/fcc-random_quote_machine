1bbb810345d64f4fc7dbe46c5a6d0cc449dfd266
Sath Sovireak <sathsovireak18@kit.edu.kh>
2019-12-23 16:08:34 +0700

    After Design

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button = ({buttonDisplayName, clickHandler})=>{
+    return(
+    <button onClick={clickHandler}>{buttonDisplayName}</button>
+    );
+}
+export default Button;
---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+// import Button from './Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faTwitter} from '@fortawesome/free-brands-svg-icons';
+
+const QuoteMachine = ({selectedQuote,assignNewQuoteIndex})=>(
+    <Card>  
+        <CardContent>
+            <Typography id="text">
+                {selectedQuote.quote} - <span id="author">{selectedQuote.author}</span>
+            </Typography> 
+        </CardContent>
+        <CardActions>
+            <Button id="new-quote" size="small" onClick={assignNewQuoteIndex}>Next Quote</Button>
+            <IconButton
+                id="tweet-quote"
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=FUNWITHQUOTE`}
+            >
+                <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+            </IconButton>
+        </CardActions>     
+    </Card>
+);
+export default QuoteMachine;
